3d1ade4dd6cd8ace56efe9c955e22535b0df0b06
Romaine Zulma <romainez@su-a3.msk.21-school.ru>
2022-04-27 20:43:13 +0300

    27.04.22

---
+++ src/romainez_s21_string.c
@@ -0,0 +1,9 @@
+#include "romainez_s21_string.h"
+#include <stdio.h>
+
+int strcmp(const char *str1, const char *str2) {
+    const char *word = strtok(str, " ");
+    while (word != NULL) {
+        if ()
+    }
+}
