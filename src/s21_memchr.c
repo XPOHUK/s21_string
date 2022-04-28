256308df4753200ec103c92e42898165f589ff14
isleanna <isleanna@student.21-school.ru>
2022-04-28 15:38:21 +0300

    s21_memchr, s21_memcmp, s21_memcpy

--- src/s21_memchr.c
+++ src/s21_memchr.c
@@ -1,8 +1,32 @@
-#include "s21_string.h"
-#include <string.h>  // Удалить при начале разработки эту заглушку
+// Copyright [2022] <isleanna>
+// #include "s21_string.h"
+#include <stdio.h>  // Библиотека для теста
 
 // Выполняет поиск первого вхождения символа c (беззнаковый тип)
 // в первых n байтах строки, на которую указывает аргумент str.
 void *s21_memchr(const void *str, int c, size_t n) {
-    *memchr(*str, c, n);  // Удалить при начале разработки эту заглушку
+    unsigned char *p;
+    size_t i;
+
+    i = 0;
+    p = (unsigned char *)str;
+    while (i < n) {
+        if (p[i] == (unsigned char)c)
+        return (p + i);
+        i++;
+    }
+    return (NULL);
+}
+
+// Тест функции
+int main() {
+    char s[] = "21school";
+    char *ptr = s21_memchr(s, 's', sizeof(s));
+    if (ptr != NULL) {
+        printf("'s' found at position %ld\n", ptr - s + 1);
+        printf("search character found: %s\n", ptr);
+    } else {
+        printf("search character not found\n");
+    }
+    return 0;
 }
