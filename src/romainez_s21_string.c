83b6af692a1b36c4dd19b72e84db042e0dbe95f6
Romaine Zulma <romainez@su-a2.msk.21-school.ru>
2022-04-30 15:22:21 +0300

    30.04.22

--- src/romainez_s21_string.c
+++ src/romainez_s21_string.c
@@ -1,8 +1,9 @@
 #include "romainez_s21_string.h"
+#include <stddef.h>
 #include <stdio.h>
 
-// Сравнивает две строки с учетом регистра. Если они идентичны, выводит 0.
-int strcmp(const char *str1, const char *str2) {
+// Сравнивает две строки с учетом регистра. Если они идентичны, выводит 0
+int s21_strcmp(const char *str1, const char *str2) {
     const char *p = str1;
     const char *q = str2;
     while (*p == *q) {
@@ -15,16 +16,42 @@ int strcmp(const char *str1, const char *str2) {
     return *(unsigned char*)p - *(unsigned char*)q;
 }
 
-// Тест
+// Сравнивает не более первых n байтов str1 и str2
+int s21_strncmp(const char *str1, const char *str2, size_t n) {
+    const unsigned char *p1;
+    const unsigned char *p2;
+    size_t i;
+
+    p1 = (const unsigned char *)str1;
+    p2 = (const unsigned char *)str2;
+    i = 0;
+    while (i < n) {
+        if (p1[i] != p2[i]) {
+            return (p1[i] - p2[i]);
+        }
+        i++;
+    }
+    return 0;
+}
+
 int main() {
+// Тест для функции s21_strcmp
     char str1[1024]= "school21";
     char str2[1024]= "school21";
-
-// Сравниваем две строки
-    if (strcmp(str1, str2) == 0) {
+    if (s21_strcmp(str1, str2) == 0) {
       printf("Строки идентичны\n");
     } else {
       printf("Строки отличаются\n");
+    }
+
+// Тест для функции s21_strncmp
+    char str11[1024] = "1234567890";  // Попробуй поменять имя переменной, посмеешься
+    char str22[1024] = "1234507890";
+    int n = 5;
+    if (s21_strncmp(str11, str22, n) == 0) {
+        printf("Первые %d символов строк идентичны\n", n);
+    } else {
+        printf("Первые %d символов строк отличаются\n", n);
     }
     return 0;
 }
--- src/romainez_s21_string.h
+++ src/romainez_s21_string.h
@@ -1,6 +1,10 @@
 #ifndef SRC_ROMAINEZ_S21_STRING_H_
 #define SRC_ROMAINEZ_S21_STRING_H_
+#include <stddef.h>
 
-int strcmp(const char *str1, const char *str2);
+// сравнивает строку str1 со строкой str2
+int s21_strcmp(const char *str1, const char *str2);
+// сравнивает не более n первых байтов str1 и str2
+int s21_strncmp(const char *str1, const char *str2, size_t n);
 
 #endif  // SRC_ROMAINEZ_S21_STRING_H_
