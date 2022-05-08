8f39d0265eb97731f4a5600184d99b24785c4806
isleanna <isleanna@student.21-school.ru>
2022-05-08 17:58:40 +0300

    main removed

--- src/s21_strpbrk.c
+++ src/s21_strpbrk.c
@@ -1,6 +1,5 @@
 // Copyright [2022] <isleanna>
 #include "s21_string.h"
-#include <stdio.h>  // Библиотека для теста
 
 // Находит первый символ в строке str1, который соответствует любому символу,
 // указанному в str2.
@@ -15,20 +14,4 @@ char *s21_strpbrk(const char *str1, const char *str2) {
         }
     }
     return (res);
-}
-
-// Тест функции
-int main() {
-    char string[] = "Functions that will make our lives easier!";
-    char chars[] = "rasf";
-    printf("Find the first matching char in '%s'\n", string);
-    printf("Chars '%s'\n", chars);
-    char *ptr = s21_strpbrk(string, chars);
-    if (ptr != NULL) {
-        printf("The first matching char within the string is '%c'\n", *ptr);
-        printf("Found at %ld\n", ptr - string + 1);
-    } else {
-        printf("No matching chars were found within the string\n");
-    }
-    return 0;
 }
