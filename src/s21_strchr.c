8b37e4f885ffd6a4879604b42363a0dcc0ea9276
XPOHUK <dmitriy@gubankov.ru>
2022-05-08 07:23:55 +0300

    style fix

--- src/s21_strchr.c
+++ src/s21_strchr.c
@@ -3,7 +3,7 @@
 // Выполняет поиск первого вхождения символа c (беззнаковый тип) в строке,
 // на которую указывает аргумент str.
 char *s21_strchr(const char *str, int c) {
-    while(*str != '\0' && *str != (char)c)
+    while (*str != '\0' && *str != (char)c)
         str++;
     return (*str == '\0' && (char)c != '\0') ? NULL : (char*)str;
 }
