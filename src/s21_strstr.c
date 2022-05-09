e330b50b72ec30c69f871848798f36aa6f732023
XPOHUK <dmitriy@gubankov.ru>
2022-05-09 08:23:42 +0300

    Remove commented code

--- src/s21_strstr.c
+++ src/s21_strstr.c
@@ -1,5 +1,3 @@
-#include <string.h>  // Удалить при начале разработки эту заглушку
-
 #include "s21_string.h"
 
 // Находит первое вхождение всей строки needle
@@ -9,22 +7,6 @@ char *s21_strstr(const char *haystack, const char *needle) {
     while (*haystack != '\0' && s21_strlen(haystack) >= s21_strlen(needle) &&
            s21_strncmp(haystack, needle, s21_strlen(needle))) {
         haystack++;
-        // if (*haystack == *needle) {
-        //     for (size_t i = 0; i < s21_strlen(needle); i++) {
-        //         if (*(haystack + i) == *(needle + i)) {
-        //             continue;
-        //         } else {
-        //             get = 1;
-        //             break;
-        //         }
-        //     }
-        //     if (!get) {
-        //         res = (char *)haystack;
-        //         break;
-        //     }
-        // }
-        // haystack++;
-        // get = 0;
     }
     if (s21_strlen(haystack) >= s21_strlen(needle)) res = (char *)haystack;
     return res;
