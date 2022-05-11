0b291df3abcbda4dec4dddb54dba5f6c686daec8
Prosser Branden <prosserb@ga-d5.msk.21-school.ru>
2022-05-11 23:26:33 +0300

    s21_strerror

--- src/s21_strerror.c
+++ src/s21_strerror.c
@@ -1,5 +1,8 @@
 #include "s21_string.h"
-#include <string.h>  // Удалить при начале разработки эту заглушку
+#include "s21_error_lib.h"
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
 
 // Выполняет поиск во внутреннем массиве номера ошибки errnum
 // и возвращает указатель на строку с сообщением об ошибке.
@@ -7,6 +10,25 @@
 // для операционных систем mac и linux.
 // Описания ошибок есть в оригинальной библиотеке.
 // Проверка текущей ОС осуществляется с помощью директив.
+
+char message[200];
+
 char *s21_strerror(int errnum) {
-    return strerror(errnum);  // Удалить при начале разработки эту заглушку
+    if ((errnum > 0) && (errnum <= ERRMAX)) {
+        s21_sprintf(message, errors[errnum]);
+    } else {
+        s21_sprintf(message, "Unknown error: %d", errnum);
+    }
+    return message;
+}
+
+
+int main() {
+    FILE *fp;
+    errno = 0;
+    fp = fopen("file", "r");
+    if (fp != NULL) {
+        printf(strerror(errno));
+     }
+    fclose(fp);
 }
