3d6a6066808d3324b7b490dde5bb0af98492669a
Johnie Tysane <johniety@ox-b4.msk.21-school.ru>
2022-05-15 13:05:42 +0300

    fix strerror for macOS

--- src/s21_strerror.c
+++ src/s21_strerror.c
@@ -1,27 +1,18 @@
-#include "s21_string.h"
-#include "s21_error_lib.h"
+#include "s21_strerror.h"
+
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
 
-char message[200];
+#include "s21_string.h"
+
+char message[MAX_MESSAGE];
 
 char *s21_strerror(int errnum) {
     if ((errnum > 0) && (errnum <= ERRMAX)) {
-       printf(message, errors[errnum]);
-       } else {
-        printf(message, "Unknown error: %d", errnum);
-      }
+        s21_strcpy(message, errors[errnum]);
+    } else {
+        s21_sprintf(message, "Undefined error: %d", errnum);
+    }
     return message;
-}
-
-
-int main() {
-    FILE *fp;
-    errno = 0;
-    fp = fopen("file", "r");
-    if (fp != NULL) {
-        printf("%s", strerror(errno));
-     }
-    fclose(fp);
 }
