810abec553f1bdc58f0acad18468ae1ba76f6283
Dmitriy Gubankov <dmitriy@gubankov.ru>
2022-05-23 09:02:50 +0300

    fix makefile

--- src/s21_memcmp.c
+++ src/s21_memcmp.c
@@ -3,15 +3,12 @@
 
 // Сравнивает первые n байтов str1 и str2.
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
-    const unsigned char *p1;
-    const unsigned char *p2;
-    s21_size_t i;
+    const unsigned char *p1 = (const unsigned char *)str1;
+    const unsigned char *p2 = (const unsigned char *)str2;
+    s21_size_t i = 0;
 
-    p1 = (const unsigned char *)str1;
-    p2 = (const unsigned char *)str2;
-    i = 0;
     while (i < n && p1[i] == p2[i]) {
         i++;
     }
-    return (n == 0) ? 0 : (p1[i] - p2[i]);
+    return (n == 0) ? 0 : (p1[i - 1] - p2[i - 1]);
 }
